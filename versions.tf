1bc3db3adf7353cbad0117222b772c9e879b3785
Dee <ddhulipalla@teampurpose.com>
2025-07-24 15:10:30 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -4,7 +4,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.0"
+      #version = ">= 6.0"
+      version = ">= 5.34, < 6.0"
+
     }
     external = {
       source  = "hashicorp/external"
